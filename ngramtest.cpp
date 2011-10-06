fbbb651d477137757c1c9599032cf3e2be8aaac2
Maarten van Gompel <proycon@anaproy.nl>
2011-10-06 19:50:18 +0200

    update

--- ngramtest.cpp
+++ ngramtest.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+const int MINTOKENS = 2;
 
 int split(string str, vector<string> &result, char delim = ' ') {
   string tmp = "";
@@ -47,43 +48,74 @@ string join(vector<string> seq, char delim = ' ') {
 
 int main( int argc, char *argv[] ) {
     
-    if (argc != 3) {
-        cerr << "Usage: ngramtest file n" << endl;
+    if (argc != 2) {
+        cerr << "Usage: ngramtest file" << endl;
         exit(2);
     }
     
     string filename = argv[1];
     cerr << "Processing " << filename << endl;
-    int n = atoi(argv[2]);
+    //int n = atoi(argv[2]);
     
     
-    ifstream *IN =  new ifstream( filename.c_str() );    
     string line;    
     unordered_map< string, int> freqlist;
     const int MAXLENGTH = 6;
 
+
     for (int n = 1; n <= MAXLENGTH; n++) {
         cerr << "Counting " << n << "-grams" << endl;
-
-        IN->seekg(0);
+        int linenum = 0;
+        int tokens = 0;
+        ifstream *IN =  new ifstream( filename.c_str() );    
         while (IN->good()) {
+            linenum++;
+            if (linenum % 10000 == 0) {
+                cerr << "\t@" << linenum << endl;
+            }
             getline(*IN, line);  
             vector<string> words;
             split(line, words);
-            
-                
+                            
             int l = words.size();        
             for (int i = 0; i < l - n + 1; i++) {
                 vector<string> ngram = vector<string>(words.begin() + i, words.begin() + i + n);
+            
                 
+                if (n > 2) {
+                    vector<string> subngram1 = vector<string>(words.begin() + i, words.begin() + i + (n - 1));
+                    string subngram1_s = join(subngram1);
+                    if (!freqlist.count(subngram1_s)) continue;
+                    
+                    vector<string> subngram2 = vector<string>(words.begin() + i + 1, words.begin() + i + n - 1);
+                    string subngram2_s = join(subngram2);
+                    if (!freqlist.count(subngram2_s)) continue;
+                }
                 
                 string ngram_s = join(ngram);            
-                //cout << ngram_s << endl;
                 freqlist[ngram_s] += 1;
-                
+                tokens++;
+                //cout << ngram_s << endl;
+            
+
+                 
             }            
             
-       } 
+       }
+       cerr << "Found " << tokens << " " << n << "-grams" << endl;
+
+       //prune n-grams
+       int pruned = 0;
+       for(unordered_map<string,int>::iterator iter = freqlist.begin(); iter != freqlist.end(); iter++ ) {
+            if (iter->second <= MINTOKENS) {
+                //if (iter->second != 0) pruned++;
+                pruned += freqlist.erase(iter->first);
+            }
+       }
+       cerr << "Pruned " << pruned << " " << n << "-grams, " << (tokens - pruned) <<  " left" << endl;
+       
+       
+ 
     }   
 
 }
