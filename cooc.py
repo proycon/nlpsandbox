5d6c0b240a3901a84f84bca4e43a07d8334ef07d
Maarten van Gompel <proycon@anaproy.nl>
2011-11-10 13:36:08 +0100

    added co-occurence based alignment computation thingie

---
+++ cooc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys
+
+sourcefile = sys.argv[1]
+targetfile = sys.argv[2]
+
+
+def loadindex(filename):
+    index = {}
+    with open(sourcefile) as f:
+        for line in f:
+            fields = line.split('\t')        
+            if fields:
+                index[fields[0]] = set(( int(x) for x in indices.strip().split(' ')))
+    return index
+    
+
+print >>sys.stderr,"Loading source index"    
+sourceindex = loadindex(sourcefile)
+print >>sys.stderr,"Loading target index"
+targetindex = loadindex(targetfile)
+
+jaccard = {}
+
+for source in sourceindex:
+    maxcooc = 0    
+    for target in targetindex:
+        if not source in jaccard:
+            jaccard[source] = {}
+        jaccard[source][target] = len(sourceindex[source] & targetindex[target]) / float(len(sourceindex[source] | targetindex[target])) #intersection / union
+        if jaccard[source][target] > maxcooc:
+            maxcooc = jaccard[source][target]
+                
+        for target in targetindex:
+            if jaccard[source][target] == maxcooc:
+                print source + '\t' + target + '\t' + str(jaccard[source][target])
+
+            
+        
+        
+            
+            
+       
+
+        
+        
+        
+        
+        
+        
+        
