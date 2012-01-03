0d096b28b3c4a046c48b1f8555e653621a44f84a
Maarten van Gompel <proycon@anaproy.nl>
2012-01-03 17:50:14 +0100

    adapted

--- localsetup.sh
+++ localsetup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "Expected one parameter: installation directory (for example: /exp/mvgompel/local/)" >&2
+	exit 1
+fi
 TARGET=$1
 cd $TARGET
 mkdir src bin etc lib share
@@ -8,7 +12,7 @@ cd Timbl6
 svn update
 bash bootstrap && ./configure --prefix=$TARGET && make && make install
 if [ $? -ne 0 ]; then
-        echo "Error during installation of Timbl"
+        echo "Error during installation of Timbl" >&2
         exit 1
 fi
 cd ..
@@ -16,7 +20,7 @@ svn co https://ilk.uvt.nl/svn/trunk/sources/TimblServer
 cd TimblServer && svn update
 sh bootstrap && ./configure --prefix=$TARGET && make && make install
 if [ $? -ne 0 ]; then
-        echo "Error during installation of TimblServer"
+        echo "Error during installation of TimblServer" >&2
         exit 1
 fi
 cd ..
@@ -32,7 +36,7 @@ svn co https://ilk.uvt.nl/svn/sources/libfolia/trunk libfolia
 cd libfolia && svn update
 sh bootstrap && ./configure --prefix=$TARGET && make && make install
 if [ $? -ne 0 ]; then
-        echo "Error during installation of libfolia"
+        echo "Error during installation of libfolia" >&2
         exit 1
 fi
 cd ..
@@ -40,7 +44,7 @@ svn co https://ilk.uvt.nl/svn/sources/ucto/trunk ucto
 cd ucto && svn update
 sh bootstrap && ./configure --prefix=$TARGET && make && make install
 if [ $? -ne 0 ]; then
-        echo "Error during installation of ucto"
+        echo "Error during installation of ucto" >&2
         exit 1
 fi
 cd ..
@@ -48,17 +52,17 @@ svn co https://ilk.uvt.nl/svn/sources/Frog/trunk Frog
 cd Frog && svn update
 sh bootstrap && ./configure --prefix=$TARGET && make && make install
 if [ $? -ne 0 ]; then
-        echo "Error during installation of Frog"
+        echo "Error during installation of Frog" >&2
         exit 1
 fi
 cd ../..
-svn co https://ilk.uvt.nl/svn/trunk/sources/pynlpl
-cd pynlpl && svn update
+git clone git://github.com/proycon/pynlpl.git
+cd pynlpl && git pull
 cd ..
-svn co https://ilk.uvt.nl/svn/trunk/sources/pbmbmt/trunk pbmbmt
-cd pbmbmt && svn update
-ln -s ../pynlpl
-./install-svn.sh
+#svn co https://ilk.uvt.nl/svn/trunk/sources/pbmbmt/trunk pbmbmt
+#cd pbmbmt && svn update
+#ln -s ../pynlpl
+#./install-svn.sh
 cd ..
 echo "All done"
 
--- xcount.py
+++ xcount.py
@@ -24,6 +24,29 @@ def addparent(ngram, parent):
             
     if add:
         parents[ngram].append(parent) 
+
+def addparentR(ngram, parent):        
+    global parents
+    if not (ngram in parents):
+        parents[ngram] = []
+    parents[ngram].append(parent) 
+        
+def totalparentoccurrence(ngram):
+    global parents
+    """Compute the occurrence of the parents"""        
+    
+    for parent in parents[ngram]:
+        for parent2 in parents[ngram]:
+            if parent != parent2:                
+                if parent in parent2:
+                    c = min(g.freqlist[parent], g.freqlist[parent2]
+                    rel[(parent,parent2)] = c
+                elif parent2 in parent:
+                    rel[(parent,parent2)] = c
+                else:
+                    
+        
+    
         
 
 g = PatternGraph2(sys.argv[1])
@@ -33,12 +56,65 @@ for ngram, count in g.freqlist.items():
     for subgram in ngram.subngrams():
         if subgram in g.freqlist:
             addparent(subgram, ngram)
-                
-                
-for ngram, parents in parents.items():
-    print str(ngram) + "\t" + " || ".join([ str(parent) for parent in parents])
-    
             
+
+xcount = {}
+n = g.max_n
+while n > 0:
+    for ngram, count in g.freqlist.items():
+        if len(ngram) == n:
+            if ngram in parents:
+                superparents = []
+                
+                dep = {}
+                  
+                parentcount = 0                            
+                for parent in parents[ngram]:
+                    for parent2 in parents[ngram]:
+                        if parent != parent2 and parent in parents and parent2 in parents:
+                            superparents = set()
+                            for superparent in parents[parent]:
+                                for superparent2 in parents[parent2]:
+                                    if superparent == superparent2:
+                                        superparents.add(superparent)
+                            if overlap:
+                                if not parent in dep:
+                                    dep[parent] = set()
+                                dep[parent].add(parent2)
+                
+                processed = {}
+                for parent in parents[ngram]:        
+                    if parent in dep:                        
+                        minoc = g.freqlist[parent]
+                        for parent2 in dep[parent]:
+                            if g.freqlist[parent2] < minoc:
+                                minoc = g.freqlist[parent2]
+                            processed[parent2] = True
+                        parentcount += minoc
+                        #print >>sys.stderr,"DEP '", str(parent) + "' ("+str(g.freqlist[parent])+ ") '" + str(parent2) + "'  ("+str(g.freqlist[parent2])+ ") = " +  str(minoc)
+                    elif not (parent in processed):    
+                        parentcount += g.freqlist[parent]                                        
+                
+                if parentcount > g.freqlist[ngram]:                        
+                    print >>sys.stderr, "NEGATIVE result for " + str(ngram)
+                    xcount[ngram] = 0
+                else:
+                    xcount[ngram] = g.freqlist[ngram] - parentcount
+            else:
+                xcount[ngram] = g.freqlist[ngram]
+    n = n - 1
+            
+                
+                
+for ngram in g.freqlist:
+    try:
+        curparents = parents[ngram]
+    except KeyError:
+        curparents = []
+    xfactor = xcount[ngram] / float(g.freqlist[ngram])
+    print str(ngram) + "\t" + str(g.freqlist[ngram]) + "\t" + str(xcount[ngram]) + "\t" + str(xfactor) + "\t" +  " || ".join([ str(parent) + " (" + str(g.freqlist[parent]) +")"  for parent in curparents])
+    
+
 
         
 
