30856fce73f98e910a8371058fcbab93f85ca875
Maarten van Gompel <proycon@anaproy.nl>
2012-10-25 22:21:43 +0200

    fix

--- xmlentityfix.py
+++ xmlentityfix.py
@@ -10,8 +10,19 @@ f = codecs.open(sys.argv[1],'r','utf-8')
 for line in f:
     for i,c in enumerate(line):
         if c == '&':
-            if line[i:5] != '&amp;':
-                o += '&amp;'
+            if line[i:i+5] != '&amp;' and line[i:i+1] != '&#':
+                #find matching semicolon?
+                entity = False
+                for c2 in line[i+1:]:
+                    if c2 == ';':
+                        entity = True
+                        break
+                    elif not c2.isalnum():
+                        break 
+                if not entity:
+                    o += '&amp;'
+                else:
+                    o += c
             else:
                 o += c        
         else:
