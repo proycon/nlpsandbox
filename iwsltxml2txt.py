10d0397778902cac20f8b1f857d85e4dbea64a85
Maarten van Gompel <proycon@anaproy.nl>
2012-12-07 20:37:28 +0100

    added

---
+++ iwsltxml2txt.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+import lxml
+import sys
+
+doc = lxml.etree.parse(sys.argv[1]).getroot()
+for e in doc.xpath('//seg'):
+    print e.text
+    
+    
+
