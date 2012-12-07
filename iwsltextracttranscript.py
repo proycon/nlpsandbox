8e1424d56860ba7d2c86c1db3f3edcb7e444fd59
Maarten van Gompel <proycon@anaproy.nl>
2012-12-07 21:11:10 +0100

    redone

--- iwsltextracttranscript.py
+++ iwsltextracttranscript.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 
-import lxml.etree
 import sys
+import codecs
 
-doc = lxml.etree.parse(sys.argv[1]).getroot()
-for e in doc.xpath('//transcript'):
-    print e.text.strip().encode('utf-8')
-    
+copy = False 
+f = codecs.open(sys.argv[1],'r')
+for line in f:
+    line = line.strip()
+    if line.strip() == "<transcript>":
+        copy = True
+    elif line.strip() == "</transcript>":
+        copy = False
+    elif copy:
+        print line
+f.close()
