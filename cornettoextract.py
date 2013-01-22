4563b16608ba9dd5994869e1697258f3e5e12798
Maarten van Gompel <proycon@anaproy.nl>
2013-01-22 17:02:43 +0100

    added

---
+++ cornettoextract.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+from cornetto.cornet import Cornet
+import sys
+
+#Usage: cornettoextract.py cornetto-lu-db cornetto-synset-db
+
+print >>sys.stderr, "Loading Cornetto DB (time and memory intensive!)"
+c = Cornet()
+c.open(sys.argv[1],sys.argv[2])
+print >>sys.stderr, "Generating Knowledge Base..."
+
