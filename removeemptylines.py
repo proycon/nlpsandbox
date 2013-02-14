29be15571538b5178dcbdb25c7fd0381a4313219
Maarten van Gompel <proycon@anaproy.nl>
2013-02-14 12:25:21 +0100

    added

---
+++ removeemptylines.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+import sys
+import os
+
+deletelines = set()
+
+
+for filename in sys.argv[1:]:
+    f = open(filename)
+    for i, line in enumerate(f):
+        line = line.strip()
+        if not line: 
+            deletelines.add(i)
+    f.close()
+    
+    
+for filename in sys.argv[1:]:
+    f_in = open(filename + '.tmp','r')
+    f_out = open(filename + '.tmp','w')    
+    for i, line in enumerate(f_in):        
+        if not i in deletelines:
+            f.write(line)
+    f_in.close()
+    f_out.close()
+    os.rename(filename + '.tmp', filename)
+    
+            
+            
