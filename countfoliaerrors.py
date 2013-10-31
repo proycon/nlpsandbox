78c671345420b290ad6ebf95b1a173b0402e9f5c
Maarten van Gompel <proycon@anaproy.nl>
2013-10-31 16:47:04 +0100

    added

---
+++ countfoliaerrors.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from pynlpl.formats import folia
+import sys
+
+try:
+    filename = sys.argv[1]
+except:
+    raise Exception("Specify a FoLiA file")
+
+doc = folia.Document(file=filename)
+count = 0
+for c in doc.select(folia.Correction):
+    print("SUGGESTIONS: " + ",".join([str(x) for x in c.suggestions()]))
+    count += 1
+print(count)
