cd2d77a862591998e477ac1a42e189563b064b0d
Maarten van Gompel <proycon@anaproy.nl>
2014-03-05 11:24:51 +0100

    added

---
+++ lem_en.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import sys
+import io
+from nltk import pos_tag
+from nltk.stem.wordnet as wordnet
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return 'a'
+    elif treebank_tag.startswith('V'):
+        return 'v'
+    elif treebank_tag.startswith('N'):
+        return 'n'
+    elif treebank_tag.startswith('R'):
+        return 'r'
+    else
+        return ''
+
+inputfile = sys.argv[1]
+
+lemmatizer.lemmatize('going', wordnet.VERB)
+lemmatizer = wordnet.WordNetLemmatizer()
+
+with io.open(intputfile + '.lem', 'w',encoding='utf-8') as f_lemma:
+    with io.open(intputfile + '.pos', 'w',encoding='utf-8') as f_pos:
+        with io.open(inputfile,'r',encoding='utf-8') as f:
+            for line in f:
+                posline = []
+                lemline = []
+                tokens = line.split(' ')
+                postags = pos_tag(tokens)
+                for token, postag in zip(tokens,postags):
+                    posline.append(postag)
+                    lemline.append(lemmatizer.lemmatize(token,postag))
+                f_lemma.write(" ".join(lemline))
+                f_pos.write(" ".join(posline))
+
+
