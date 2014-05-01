7cb4283a004851e59530060c3718d650a7a93a0c
Maarten van Gompel <proycon@anaproy.nl>
2014-05-01 12:49:37 +0200

    update

--- nestednameddict.py
+++ nestednameddict.py
@@ -2,10 +2,10 @@
 class NestedNamedDict():
     def __init__(self):
         self._node = {}
+        self._list = False
 
     def __setattr__(self, attrib, value):
         if attrib[0] != '_':
-            print("Setting attrib: ", attrib)
             self._node[attrib] = value
         else:
             return super(NestedNamedDict,self).__setattr__(attrib, value)
@@ -18,11 +18,30 @@ class NestedNamedDict():
         else:
             return super(NestedNamedDict,self).__getattr__(attrib)
 
+    def __getitem__(self, index):
+        if isinstance(index,int):
+            if not index in self._node:
+                self._node[index] = NestedNamedDict()
+                self._node[index]._list = True
+            return self._node[index]
+        else:
+            return self.__getattr__(index)
+
+    def __setitem__(self, index, value):
+        if isinstance(index,int):
+            self._node[index] = value
+        else:
+            return self.__setattr__(index, value)
+
     def __len__(self):
         return len(self._node)
 
     def __iter__(self):
-        return iter(self._node)
+        if self._list:
+            return sorted(iter(self._node))
+        else:
+            return iter(self._node)
+
 
 if __name__ == '__main__':
 
@@ -33,5 +52,13 @@ if __name__ == '__main__':
     print(d.blah.blieh.blo)
     print(d.blah)
 
+    d.blah.l[0] = 'a'
+    d.blah.l[1] = 'b'
+    d.blah.l[2].x = 'c'
+
     for name in d.blah:
         print(name)
+
+    for name in d.blah.l:
+        print(name)
+
