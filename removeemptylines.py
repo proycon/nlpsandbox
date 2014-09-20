64bc75ed32158cac4b40eba9e0bc3d7bf9c75c86
Maarten van Gompel <proycon@anaproy.nl>
2014-09-20 14:16:13 +0200

    remove lines with \r as well

--- removeemptylines.py
+++ removeemptylines.py
@@ -11,19 +11,19 @@ for filename in sys.argv[1:]:
     f = open(filename)
     for i, line in enumerate(f):
         line = line.strip()
-        if not line: 
+        if not line or '\r' in line:
             deletelines.add(i)
     f.close()
-    
-    
+
+
 for filename in sys.argv[1:]:
     f_in = open(filename,'r')
-    f_out = open(filename + '.tmp','w')    
-    for i, line in enumerate(f_in):        
+    f_out = open(filename + '.tmp','w')
+    for i, line in enumerate(f_in):
         if not i in deletelines:
             f_out.write(line)
     f_in.close()
     f_out.close()
     os.rename(filename + '.tmp', filename)
-    
-            
+
+
