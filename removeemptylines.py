4dc3312e0fd7fc257947cacb3a5092c8f9d63005
Maarten van Gompel <proycon@anaproy.nl>
2014-09-20 14:22:33 +0200

    update

--- removeemptylines.py
+++ removeemptylines.py
@@ -21,7 +21,10 @@ for filename in sys.argv[1:]:
     f_out = open(filename + '.tmp','w')
     for i, line in enumerate(f_in):
         if not i in deletelines:
-            f_out.write(line)
+            if line[-2:] == '\r\n':
+                f_out.write(line[:-2] + '\n')
+            else:
+                f_out.write(line)
     f_in.close()
     f_out.close()
     os.rename(filename + '.tmp', filename)
