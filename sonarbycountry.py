b864316af22eeac286c8eca729c7b3f84ac2f8b3
Maarten van Gompel <proycon@anaproy.nl>
2014-12-09 12:32:43 +0100

    added

---
+++ sonarbycountry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import lxml.etree
+
+
+index = {}
+
+print("Indexing",file=sys.stderr)
+for root, dirs, files in os.walk(sys.argv[1]):
+    print("Processing " + root + "...",file=sys.stderr)
+    for f in files:
+        f = root + f
+        if f[-9:] == ".cmdi.xml":
+            foliafile = f[:-9] + ".folia.xml"
+            if not os.path.exists(foliafile):
+                print("WARNING: Missing FoLiA for " + f,file=sys.stderr)
+                continue
+            doc = lxml.etree.parse(f).getroot()
+            for element in doc:
+                #element.tag, element.attrib['blah'], element.text
+                pass
+
+            country = False
+            for element in doc.xpath("//cmd:CMD/cmd::Components/cmd:SoNaRcorpus/Source/Country",namespaces={'cmd':"http://www.clarin.eu/cmd/"}):
+                country = element.text()
+                pass
+
+            country = country.strip.lower()
+            if not country in index:
+                index[country] = []
+
+            index[country].append(foliafile)
+    print("... index now contains " + str(sum( [ len(x) for x in index.values() ])) + " files" ,file=sys.stderr)
+
+
+
+
