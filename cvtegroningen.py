bec6ca74ceb5324200ae646199ac5401b067b8bf
Maarten van Gompel <proycon@anaproy.nl>
2015-06-01 16:09:19 +0200

    update

--- cvtegroningen.py
+++ cvtegroningen.py
@@ -28,6 +28,7 @@ def processfile(filename):
 
     corrections = []
     gaps=[]
+    inlinegaps=[]
     correctionbuffer = ""
     gapbuffer=""
     incorrection = False
@@ -37,16 +38,26 @@ def processfile(filename):
     nospace = False
     docid = os.path.basename(tmpfilename)
     score = None
+    begin = 0
+    skipchar = 0
 
     with open(filename,'r',encoding='iso-8859-15') as f, open(tmpfilename,'w',encoding='utf-8') as f_out:
-        for i, line in enumerate(f):
+        for linenum, line in enumerate(f):
             newline = ""
+            if linenum == 0:
+                m =  re.match(r"([%w%d_]+) \(score (%d+)\)", line)
+                if m:
+                    docid = m.group(1)
+                    score = m.group(2)
             m =  re.match("(%d+) min; (%d+) words)", line)
             if m:
                 mins = m.group(1)
                 words = m.group(2)
             else:
                 for i, c in enumerate(line):
+                    if skipchar:
+                        skipchar = skipchar - 1
+                        continue
                     if c == '#':
                         if incorrection:
                             incorrection = False
@@ -59,26 +70,43 @@ def processfile(filename):
                         ingap = True
                         gapbuffer = ""
                         if i > 0:
-                            nospace = c[i-1]
+                            nospace = (c[i-1] == " ")
+                            begin = i-1
                         else:
                             nospace = False
                     elif c == "]":
                         if ingap:
                             if nospace and i +1 < len(line) and c[i+1].isalnum():
                                 #gap is in the middle of a word
-                                #TODO
-
-                            ingap = False
-                            newline += "%G" + str(len(gaps)) + "%" #placeholder
-                            gaps.append(gapbuffer)
+                                ingap = False
+                                newline += "%I" + str(len(inlinegaps)) + "%" #placeholder
+                                left = ""
+                                for j in range(begin,0,-1):
+                                    if line[j].isalpha():
+                                        left = line[j] + left
+                                right = ""
+                                for j in range(i+1,len(line)):
+                                    if line[j].isalpha():
+                                        right = right + line[j]
+                                inlinegaps.append((line,gapbuffer,right))
+                            else:
+                                ingap = False
+                                newline += "%G" + str(len(gaps)) + "%" #placeholder
+                                gaps.append(gapbuffer)
                         else:
                             newline += c
+                    elif c == "\\" and i < len(line) - 1 and c[i+1] == "\\":
+                        skipchar = 1
+                        newline += "%C" + str(len(corrections)) + "%" #placeholder
+                        corrections.append( ('','.') )
+                        newline += " %H%" #placeholder triggering capitalization of next word
                     elif incorrection:
                         correctionbuffer += c
                     elif ingap:
                         gapbuffer += c
                     else:
                         newline += c
+                newline += "%B%"
                 f_out.write(newline)
 
     tokenizer = ucto.Tokenizer("/home/proycon/lamachine/etc/ucto/tokconfig-nl-withplaceholder",xmloutput=True,docid=docid)
@@ -93,6 +121,10 @@ def processfile(filename):
         foliadoc.metadata['words'] = words
     if mins is not None:
         foliadoc.metadata['annotationtime'] = mins
+    if score is not None:
+        foliadoc.metadata['score'] = score
+    foliadoc.metadata['school'] = " ".join(os.path.basename(os.path.dirname(tmpfilename)).split(' ')[:-2])
+    foliadoc.metadata['class'] = os.path.basename(os.path.dirname(tmpfilename)).split(' ')[-1]
 
     #remove text-content on sentences (will contain placeholders and has no added value):
     for sentence in foliadoc.sentences():
@@ -103,7 +135,15 @@ def processfile(filename):
     for word in foliadoc.words():
         if word.cls == "PLACEHOLDER":
             doc = word.doc
-            if str(word)[1] == "C":
+            if str(word)[1] == "H":
+                nextword = word.next(folia.Word)
+                if nextword:
+                    originaltext = str(nextword)
+                    newtext = str(nextword)[0].upper() + str(nextword)[1:]
+                    correction = nextword.correct(new=newtext,cls='capitalizationerror')
+                    correction.original().settext(originaltext)
+                word.parent.remove(word)
+            elif str(word)[1] == "C":
                 index = int(str(word)[2:-1])
                 originaltext,newtext = corrections[index]
                 if all( not c.isalnum() for c in newtext ):
@@ -154,6 +194,15 @@ def processfile(filename):
                     word.parent.data[index] = folia.Gap(doc, cls='cancelled')
                 else:
                     word.parent.data[index] = folia.Gap(doc,content=gapcontent, cls='cancelled')
+            elif str(word)[1] == "I":
+                index = int(str(word)[2:-1])
+                left, gapcontent, right = inlinegaps[index]
+            elif str(word)[1] == "B":
+                index = word.parent.getindex(word)
+                word.parent.data[index] = folia.Linebreak(doc)
+
+
+
 
     foliadoc.save()
 
