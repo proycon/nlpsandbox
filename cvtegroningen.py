1715912f02d1c61e848e772547cc388098f7d8a0
Maarten van Gompel <proycon@anaproy.nl>
2015-06-01 16:56:13 +0200

    updae

--- cvtegroningen.py
+++ cvtegroningen.py
@@ -39,70 +39,71 @@ def processfile(filename):
     with open(filename,'r',encoding='iso-8859-15') as f, open(tmpfilename,'w',encoding='utf-8') as f_out:
         for linenum, line in enumerate(f):
             newline = ""
-            if linenum == 0:
-                m =  re.match(r"([%w%d_]+) \(score (%d+)\)", line)
-                if m:
-                    docid = m.group(1)
-                    score = m.group(2)
-            m =  re.match(r"\((%d+) min; (%d+) words\)", line)
-            if m:
-                mins = m.group(1)
-                words = m.group(2)
-            else:
-                for i, c in enumerate(line):
-                    if skipchar:
-                        skipchar = skipchar - 1
-                        continue
-                    if c == '#':
-                        if incorrection:
-                            incorrection = False
-                            newline += "%C" + str(len(corrections)) + "%" #placeholder
-                            corrections.append( tuple(correctionbuffer.split('~')) )
-                        else:
-                            incorrection = True
-                            correctionbuffer = ""
-                    elif c == "[":
-                        ingap = True
-                        gapbuffer = ""
-                        if i > 0:
-                            nospace = (line[i-1] == " ")
-                            begin = i-1
-                        else:
-                            nospace = False
-                    elif c == "]":
-                        if ingap:
-                            if nospace and i +1 < len(line) and line[i+1].isalnum():
-                                #gap is in the middle of a word
-                                ingap = False
-                                newline += "%I" + str(len(inlinegaps)) + "%" #placeholder
-                                left = ""
-                                for j in range(begin,0,-1):
-                                    if line[j].isalpha():
-                                        left = line[j] + left
-                                right = ""
-                                for j in range(i+1,len(line)):
-                                    if line[j].isalpha():
-                                        right = right + line[j]
-                                inlinegaps.append((line,gapbuffer,right))
-                            else:
-                                ingap = False
-                                newline += "%G" + str(len(gaps)) + "%" #placeholder
-                                gaps.append(gapbuffer)
-                        else:
-                            newline += c
-                    elif c == "\\" and i < len(line) - 1 and line[i+1] == "\\":
-                        skipchar = 1
+            strippedline = line.strip()
+            if linenum == 0 and strippedline.find('score') != -1:
+                docid = strippedline.split(' ')[0]
+                score = strippedline.split(' ')[-1][:-1]
+                continue
+            if strippedline and strippedline[0] == '(' and strippedline[-1] == ')' and strippedline.find('min') != -1 and strippedline.find('words') != -1:
+                fields = strippedline[1:-1].split(' ')
+                mins = fields[0]
+                words = fields[2]
+                continue
+
+            for i, c in enumerate(line):
+                if skipchar:
+                    skipchar = skipchar - 1
+                    continue
+                if c == '#':
+                    if incorrection:
+                        incorrection = False
                         newline += "%C" + str(len(corrections)) + "%" #placeholder
-                        corrections.append( ('','.') )
-                        newline += " %H%" #placeholder triggering capitalization of next word
-                    elif incorrection:
-                        correctionbuffer += c
-                    elif ingap:
-                        gapbuffer += c
+                        corrections.append( tuple(correctionbuffer.split('~')) )
+                    else:
+                        incorrection = True
+                        correctionbuffer = ""
+                elif c == "[":
+                    ingap = True
+                    gapbuffer = ""
+                    if i > 0:
+                        nospace = (line[i-1] == " ")
+                        begin = i-1
+                    else:
+                        nospace = False
+                elif c == "]":
+                    if ingap:
+                        if nospace and i +1 < len(line) and line[i+1].isalnum():
+                            #gap is in the middle of a word
+                            ingap = False
+                            newline += "%I" + str(len(inlinegaps)) + "%" #placeholder
+                            left = ""
+                            for j in range(begin,0,-1):
+                                if line[j].isalpha():
+                                    left = line[j] + left
+                            right = ""
+                            for j in range(i+1,len(line)):
+                                if line[j].isalpha():
+                                    right = right + line[j]
+                            inlinegaps.append((line,gapbuffer,right))
+                        else:
+                            ingap = False
+                            newline += "%G" + str(len(gaps)) + "%" #placeholder
+                            gaps.append(gapbuffer)
                     else:
                         newline += c
-                newline += "%B%"
-                f_out.write(newline)
+                elif c == "\\" and i < len(line) - 1 and line[i+1] == "\\":
+                    skipchar = 1
+                    newline += "%C" + str(len(corrections)) + "%" #placeholder
+                    corrections.append( ('','.') )
+                    newline += " %H%" #placeholder triggering capitalization of next word
+                elif incorrection:
+                    correctionbuffer += c
+                elif ingap:
+                    gapbuffer += c
+                else:
+                    newline += c
+            if newline: newline += "%B%"
+            f_out.write(newline)
 
     tokenizer = ucto.Tokenizer("/home/proycon/lamachine/etc/ucto/tokconfig-nl-withplaceholder",xmloutput=True,docid=docid)
     tokenizer.tokenize(tmpfilename, foliafilename)
@@ -118,8 +119,17 @@ def processfile(filename):
         foliadoc.metadata['annotationtime'] = mins
     if score is not None:
         foliadoc.metadata['score'] = score
-    foliadoc.metadata['school'] = " ".join(os.path.basename(os.path.dirname(tmpfilename)).split(' ')[:-2])
-    foliadoc.metadata['class'] = os.path.basename(os.path.dirname(tmpfilename)).split(' ')[-1]
+
+    dirname = os.path.basename(os.path.dirname(filename))
+    print(filename,"--",dirname)
+    foliadoc.metadata['school'] = " ".join(dirname.split(' ')[:-2])
+    foliadoc.metadata['class'] = dirname.split(' ')[-1]
+
+    #remove text-content on paragraphs (will contain placeholders and has no added value):
+    for paragraph in foliadoc.paragraphs():
+        for i, item in enumerate(paragraph.data):
+            if isinstance(item, folia.TextContent):
+                del paragraph.data[i]
 
     #remove text-content on sentences (will contain placeholders and has no added value):
     for sentence in foliadoc.sentences():
@@ -188,10 +198,13 @@ def processfile(filename):
                 if all( ( c == '$' for c in gapcontent) ):
                     word.parent.data[index] = folia.Gap(doc, cls='cancelled')
                 else:
-                    word.parent.data[index] = folia.Gap(doc,content=gapcontent, cls='cancelled')
+                    word.parent.data[index] = folia.Gap(doc, folia.Content(doc, value=gapcontent), cls='cancelled')
             elif str(word)[1] == "I":
                 index = int(str(word)[2:-1])
                 left, gapcontent, right = inlinegaps[index]
+                word.settext(left + right)
+                word.add(folia.TextContent, left, folia.TextMarkupGap(doc, gapcontent), right, cls='original')
+                word.cls = "WORD"
             elif str(word)[1] == "B":
                 index = word.parent.getindex(word)
                 word.parent.data[index] = folia.Linebreak(doc)
