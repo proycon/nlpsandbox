f5a4e9ebd2ec450275f706ef89f70ef034fb1ee1
Maarten van Gompel <proycon@anaproy.nl>
2015-06-02 20:24:21 +0200

    fix

--- cvtegroningen.py
+++ cvtegroningen.py
@@ -105,7 +105,7 @@ def processfile(filename,parseonly=False):
                     ingap = True
                     gapbuffer = ""
                     if i > 0:
-                        nospace = (line[i-1] == " ")
+                        nospace = (line[i-1] != " ")
                         begin = i-1
                     else:
                         nospace = False
