ce6dac11966cd8b0ef27869a69f31fb83d10760c
Maarten van Gompel <proycon@anaproy.nl>
2016-03-10 20:12:09 +0100

    fix

--- subtiel2txt.py
+++ subtiel2txt.py
@@ -20,10 +20,16 @@ for i, doc in enumerate(folia.Corpus(corpusdir, 'xml.gz','', lambda fn: os.path.
         for a in ca.select(folia.Alignment):
             sentencepair[a.cls] = " ".join([ aref.t for aref in a.select(folia.AlignReference,ignore=False)])
         if all([lang in sentencepair for lang in langs]):
+            if any([ not text.strip() for text in sentencepair.values()]):
+                print("  WARNING: Empty text", text,file=sys.stderr)
+                continue
+            if any([ '\n' in text for text in sentencepair.values()]):
+                print("  WARNING: Newline in text: ", text,file=sys.stderr)
+                continue
             for lang, text in sentencepair.items():
                 outfiles[lang].write(text+"\n")
         else:
-            print("WARNING: Missing language in sentencepair ", sentencepair,file=sys.stderr)
+            print("  WARNING: Missing language in sentencepair ", sentencepair,file=sys.stderr)
 
 
 
