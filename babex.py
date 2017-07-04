59dd5c9b5b01bb8d2e9e873094281bbb9ec9e1c5
Maarten van Gompel <proycon@anaproy.nl>
2017-07-04 12:34:25 +0200

    fixes

--- babex.py
+++ babex.py
@@ -8,27 +8,36 @@ for filename in sys.argv[1:]:
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             line = line.strip()
-            if not (line[0] == "<" and line[-1] == ">"):
+            if line and not (line[0] == "<" and line[-1] == ">"):
                 fields = line.split('\t')
                 if len(fields) != 3:
                     print("WARNING: Unexpected column layout:", fields,file=sys.stderr)
                 else:
                     text, pos, lemma = fields
+                    cutoff = 0
                     for i, c in enumerate(reversed(text)):
                         if c.isalnum():
                             cutoff = -1 * i
+                            break
                     if cutoff != 0:
                         punct = text[cutoff:]
                         text = text[:cutoff]
+                        if len(punct) > 0:
+                            homogenous = True
+                            for pc in punct[1:]:
+                                if pc != punct[0]:
+                                    homogenous = False
+                            if homogenous:
+                                punct = punct[0]
                         data.append( (text,pos,lemma) )
                         data.append( (punct,"PUNC",punct) )
                     else:
                         data.append( (text,pos,lemma) )
 
-    begin = i
+    begin = 0
     for i, (text,pos, lemma) in enumerate(data):
         print(text + "\t" + lemma + "\t" +  pos)
-        if text == '.':
+        if text in ('.','?','!'):
             #decide if this is an end-of-sentence
             if i == len(data):
                 eos = True
