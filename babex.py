c2b07a38aa2668017c167aab163277025ec84b52
Maarten van Gompel <proycon@anaproy.nl>
2017-07-11 16:26:09 +0200

    fixes

--- babex.py
+++ babex.py
@@ -2,11 +2,6 @@
 
 import lxml.etree
 import sys
-for element in doc:
-    #element.tag, element.attrib['blah'], element.text
-    pass
-for element in doc.xpath("//test"):
-    pass
 
 ns = {'tei':"http://www.tei-c.org/ns/1.0"}
 
@@ -15,31 +10,45 @@ for filename in sys.argv[1:]:
     data = []
     with open(filename, 'r', encoding='utf-8') as f:
         doc = lxml.etree.parse(filename).getroot()
-        for word in doc.xpath("//tei:w",namespaces=ns):
-            text = word.text
-            pos = word.attrib['type']
-            lemma = word.attrib['lemma']
-            if all( (c.isdigit() for c in text )): lemma = text
-            if pos != 'UNRESOLVED' and lemma != 'UNRESOLVED' and word.attrib['xtype'] != 'multiw':
-                cutoff = 0
-                for i, c in enumerate(reversed(text)):
-                    if c.isalnum():
-                        cutoff = -1 * i
-                        break
-                if cutoff != 0:
-                    punct = text[cutoff:]
-                    text = text[:cutoff]
-                    if len(punct) > 0:
-                        homogenous = True
-                        for pc in punct[1:]:
-                            if pc != punct[0]:
-                                homogenous = False
-                        if homogenous:
-                            punct = punct[0]
-                    data.append( (text,pos,lemma) )
-                    data.append( (punct,"PUNC",punct) )
-                else:
-                    data.append( (text,pos,lemma) )
+        for par in doc.xpath("//tei:p",namespaces=ns):
+            for word in par.xpath("./*",namespaces=ns):
+                if word.tag == "{http://www.tei-c.org/ns/1.0}pc":
+                    text = "".join(word.xpath(".//text()"))
+                    data.append( (text,'PUNC',text) )
+                elif word.tag == "{http://www.tei-c.org/ns/1.0}w":
+                    text = "".join(word.xpath(".//text()"))
+                    if not text:
+                        print("WARNING: word has no text: " , lxml.etree.tostring(word), file=sys.stderr)
+                        continue
+                    if 'type' not in word.attrib:
+                        print("WARNING: word has no pos: ",  lxml.etree.tostring(word),file=sys.stderr)
+                        continue
+                    pos = word.attrib['type']
+                    if 'lemma' not in word.attrib:
+                        print("WARNING: word has no lemma: " , lxml.etree.tostring(word),file=sys.stderr)
+                        continue
+                    lemma = word.attrib['lemma']
+                    if all( (c.isdigit() for c in text )): lemma = text
+                    if pos != 'UNRESOLVED' and lemma != 'UNRESOLVED' and ('xtype' not in word.attrib or word.attrib['xtype'] != 'multiw'):
+                        cutoff = 0
+                        for i, c in enumerate(reversed(text)):
+                            if c.isalnum():
+                                cutoff = -1 * i
+                                break
+                        if cutoff != 0:
+                            punct = text[cutoff:]
+                            text = text[:cutoff]
+                            if len(punct) > 0:
+                                homogenous = True
+                                for pc in punct[1:]:
+                                    if pc != punct[0]:
+                                        homogenous = False
+                                if homogenous:
+                                    punct = punct[0]
+                            data.append( (text,pos,lemma) )
+                            data.append( (punct,"PUNC",punct) )
+                        else:
+                            data.append( (text,pos,lemma) )
 
     begin = 0
     for i, (text,pos, lemma) in enumerate(data):
