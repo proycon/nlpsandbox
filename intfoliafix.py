f8fef848b093f4a8d9f6c0254e4f59e5061acf1e
Maarten van Gompel <proycon@anaproy.nl>
2018-05-22 15:10:06 +0200

    added

---
+++ intfoliafix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+mergelines = []
+for filename in sys.argv[1:]:
+    with open(filename,'r',encoding='utf-8') as f:
+        found = None
+        batchlines = []
+        for i, line in enumerate(f):
+            if line.strip().startswith("<t-"):
+                if found == i-1:
+                    batchlines.append(i)
+                else:
+                    batchlines = []
+                    mergelines.append(batchlines)
+                found = i
+
+for lines in mergelines:
+    print([ str(i+1) for i in mergelines],file=sys.stderr)
