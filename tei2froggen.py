ef0b8d911bf78fe8d428fa60f1b46e27bdabaf55
Maarten van Gompel <proycon@anaproy.nl>
2018-06-15 16:45:53 +0200

    fixes

--- tei2froggen.py
+++ tei2froggen.py
@@ -13,24 +13,26 @@ for filename in sys.argv[1:]:
     for word in doc.xpath("//tei:w", namespaces=ns):
         eos = False
         text = "".join(word.itertext()).strip()
-        tail = ""
-        while text[-1] in ('.','?',',',';') and any(c.isalpha() for c in text): #strip trailing punctiation
-            tail = text[-1] + tail
-            text = text[:-1]
-        lemma = word.attrib['lemma'].lower() if 'lemma' in word.attrib else text.lower()
-        if lemma[-1] == '?': lemma = lemma[:-1]
-        if '/' in lemma: lemma = lemma.split('/')[0] #we can't deal with disjunctions! just pick the first one
-        pos = word.attrib['msd']
-        print(text + "\t" + lemma + "\t" + pos)
-        if tail:
-            print(tail + "\t" + tail + "\t" + "LET()")
-            if tail[-1] in ('.','?'):
-                #just assume end-of-sentence
+        if text:
+            tail = ""
+            while text[-1] in ('.','?',',',';') and any(c.isalpha() for c in text): #strip trailing punctiation
+                tail = text[-1] + tail
+                text = text[:-1]
+            if text:
+                lemma = word.attrib['lemma'].lower() if 'lemma' in word.attrib else text.lower()
+                if lemma[-1] == '?': lemma = lemma[:-1]
+                if '/' in lemma: lemma = lemma.split('/')[0] #we can't deal with disjunctions! just pick the first one
+                pos = word.attrib['msd']
+                print(text + "\t" + lemma + "\t" + pos)
+            if tail:
+                print(tail + "\t" + tail + "\t" + "LET()")
+                if tail[-1] in ('.','?'):
+                    #just assume end-of-sentence
+                    print("<utt>")
+                    eos = True
+            elif text and text[-1] in ('.','?'):
                 print("<utt>")
                 eos = True
-        elif text[-1] in ('.','?'):
-            print("<utt>")
-            eos = True
     if not eos:
         print("<utt>")
 
