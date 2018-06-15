767c4108320ac9c00dd131868a4c1c6a1ece948f
Maarten van Gompel <proycon@anaproy.nl>
2018-06-15 16:41:21 +0200

    added TEI 2 tsv for froggen

---
+++ tei2froggen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import lxml.etree
+
+
+ns = {"tei": "http://www.tei-c.org/ns/1.0"}
+
+eos = True
+for filename in sys.argv[1:]:
+    print("Processing " + filename,file=sys.stderr)
+    doc = lxml.etree.parse(filename).getroot()
+    for word in doc.xpath("//tei:w", namespaces=ns):
+        eos = False
+        text = "".join(word.itertext()).strip()
+        tail = ""
+        while text[-1] in ('.','?',',',';') and any(c.isalpha() for c in text): #strip trailing punctiation
+            tail = text[-1] + tail
+            text = text[:-1]
+        lemma = word.attrib['lemma'].lower() if 'lemma' in word.attrib else text.lower()
+        if lemma[-1] == '?': lemma = lemma[:-1]
+        if '/' in lemma: lemma = lemma.split('/')[0] #we can't deal with disjunctions! just pick the first one
+        pos = word.attrib['msd']
+        print(text + "\t" + lemma + "\t" + pos)
+        if tail:
+            print(tail + "\t" + tail + "\t" + "LET()")
+            if tail[-1] in ('.','?'):
+                #just assume end-of-sentence
+                print("<utt>")
+                eos = True
+        elif text[-1] in ('.','?'):
+            print("<utt>")
+            eos = True
+    if not eos:
+        print("<utt>")
+
